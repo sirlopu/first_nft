11caf1a3ddb039f4ae44facd8579e6eb65513b73
Gabriel S <sirlopug@gmail.com>
2022-03-23 15:55:42 -0700

    upload image and metadata to ipfs

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -6,11 +6,11 @@ import os
 import json
 import requests
 
-# breed_to_image_uri = {
-#     "PUG": "https://ipfs.io/ipfs/QmSsYRx3LpDAb1GZQm7zZ1AuHZjfbPkD6J7s9r41xu1mf8?filename=pug.png",
-#     "SHIBA_INU": "https://ipfs.io/ipfs/QmYx6GsYAKnNzZ9A6NvEKV9nf1VaDzJrqDR23Y8YSkebLU?filename=shiba-inu.png",
-#     "ST_BERNARD": "https://ipfs.io/ipfs/QmUPjADFGEKmfohdTaNcWhp7VGk26h5jXDA7v3VtTnTLcW?filename=st-bernard.png",
-# }
+breed_to_image_uri = {
+    "PUG": "https://ipfs.io/ipfs/QmSsYRx3LpDAb1GZQm7zZ1AuHZjfbPkD6J7s9r41xu1mf8?filename=pug.png",
+    "SHIBA_INU": "https://ipfs.io/ipfs/QmYx6GsYAKnNzZ9A6NvEKV9nf1VaDzJrqDR23Y8YSkebLU?filename=shiba-inu.png",
+    "ST_BERNARD": "https://ipfs.io/ipfs/QmUPjADFGEKmfohdTaNcWhp7VGk26h5jXDA7v3VtTnTLcW?filename=st-bernard.png",
+}
 
 
 def main():
@@ -26,29 +26,22 @@ def main():
         if Path(metadata_file_name).exists():
             print(f"{metadata_file_name} already exists! Delete it to overwrite")
         else:
-            # print(f"Creating Metadata file: {metadata_file_name}")
-            # collectible_metadata["name"] = breed
-            # collectible_metadata["description"] = f"An adorable {breed} pup!"
-            # image_path = "./img/" + breed.lower().replace("_", "_") + ".png"
-            # # print(collectible_metadata)
-            # # print(image_path)
-            # image_uri = upload_to_ipfs(image_path)
             print(f"Creating Metadata file: {metadata_file_name}")
             collectible_metadata["name"] = breed
             collectible_metadata["description"] = f"An adorable {breed} pup!"
             image_path = "./img/" + breed.lower().replace("_", "-") + ".png"
 
             image_uri = None
-            # if os.getenv("UPLOAD_IPFS") == "true":
-            #     image_uri = upload_to_ipfs(image_path)
-            # image_uri = upload_to_ipfs(image_path)
-            # image_uri = image_uri if image_uri else breed_to_image_uri[breed]
-            image_uri = upload_to_ipfs(image_path)
-            # collectible_metadata["image"] = image_uri
-            # with open(metadata_file_name, "w") as file:
-            #     json.dump(collectible_metadata, file)
-            # if os.getenv("UPLOAD_IPFS") == "true":
-            #     upload_to_ipfs(metadata_file_name)
+            if os.getenv("UPLOAD_IPFS") == "true":
+                #     image_uri = upload_to_ipfs(image_path)
+                image_uri = upload_to_ipfs(image_path)
+            image_uri = image_uri if image_uri else breed_to_image_uri[breed]
+
+            collectible_metadata["image"] = image_uri
+            with open(metadata_file_name, "w") as file:
+                json.dump(collectible_metadata, file)
+            if os.getenv("UPLOAD_IPFS") == "true":
+                upload_to_ipfs(metadata_file_name)
 
 
 def upload_to_ipfs(filepath):
@@ -60,10 +53,8 @@ def upload_to_ipfs(filepath):
         # ipfs_url = (
         #     os.getenv("IPFS_URL") if os.getenv("IPFS_URL") else "http://127.0.0.1:5001"
         # )
-        # response = requests.post(ipfs_url + "/api/v0/add", files={"file": image_binary})
         response = requests.post(ipfs_url + endpoint, files={"file": image_binary})
         ipfs_hash = response.json()["Hash"]
-        # print(ipfs_hash)
         # "./img/0-PUG.png" -> "0-PUG.png"
         filename = filepath.split("/")[-1:][0]
         # print(filename)
