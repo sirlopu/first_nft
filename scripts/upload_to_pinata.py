748e5e0d76e2c48db1495d3a6e8443acea853579
Gabriel S <sirlopug@gmail.com>
2022-03-23 14:51:22 -0700

    added pinata upload script

---
+++ scripts/upload_to_pinata.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+import requests
+
+PINATA_BASE_URL = "https://api.pinata.cloud/"
+endpoint = "pinning/pinFileToIPFS"
+# can be modified
+filepath = "./img/pug.png"
+filename = filepath.split("/")[-1:][0]
+headers = {
+    "pinata_api_key": os.getenv("PINATA_API_KEY"),
+    "pinata_secret_api_key": os.getenv("PINATA_API_SECRET"),
+}
+
+
+def main():
+    with Path(filepath).open("rb") as fp:
+        image_binary = fp.read()
+        response = requests.post(
+            PINATA_BASE_URL + endpoint,
+            files={"file": (filename, image_binary)},
+            headers=headers,
+            timeout=10,
+        )
+        print(response.json())
+        response.close()
